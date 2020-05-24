c3b008af1edb9bd64a4fad2251ad5210e1189ce5
Cindy Michelle <cindy@kodefox.com>
2020-05-25 01:10:44 +0700

    Updates

---
+++ precache-manifest.55dfbca37a275e82fe963a89c6530a50.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "afd5733ac789ab1795f1ed7a26e1ecd3",
+    "url": "/istar/favicon-16.png"
+  },
+  {
+    "revision": "cf4593bafc94eed73d24",
+    "url": "/istar/static/js/app.13cf12f4.chunk.js"
+  },
+  {
+    "revision": "e9425f561449b0b8df5a",
+    "url": "/istar/static/js/runtime~app.5624c475.js"
+  },
+  {
+    "revision": "35cb9586e535bdb537c5bc0965db671e",
+    "url": "/istar/static/media/upload_placeholder.35cb9586.png"
+  },
+  {
+    "revision": "01c8f49e1f1c2784bcc5",
+    "url": "/istar/static/js/2.0a6b262c.chunk.js"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/istar/serve.json"
+  },
+  {
+    "revision": "0d12ed5726cf680f5d5aff41de2f399a",
+    "url": "/istar/register-service-worker.js"
+  },
+  {
+    "revision": "0a0b875511d68de61f96b9bfef29f288",
+    "url": "/istar/manifest.json"
+  },
+  {
+    "revision": "448340674f54c9fa7f2fee84d72071ca",
+    "url": "/istar/index.html"
+  },
+  {
+    "revision": "5e52768b0f7cbb62cbe07751add33101",
+    "url": "/istar/favicon.ico"
+  },
+  {
+    "revision": "3d85359b37ca7590f8e91d593d02ddaf",
+    "url": "/istar/favicon-32.png"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/istar/expo-service-worker.js"
+  }
+];
